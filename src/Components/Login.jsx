9b4576fef3f311643d5820ff34f064f441ae66d8
arkajyoti-roy <arkajyotiroy445@gmail.com>
2024-09-10 00:16:19 +0530

    update Dis.css, Login.jsx and Registration.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,95 +1,98 @@
 // import React from 'react'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { useEffect } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import {auth} from "./firebase"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import {  } from 'react-router-dom';
+import { auth } from "./firebase";
+import "./Dis.css";
 
 const Login = () => {
-
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const navigate = useNavigate();
 
 
-    const request = async(e) =>{
-      e.preventDefault();
-      try {
-        await signInWithEmailAndPassword(auth, email, password)
-    console.log("Success");
-    toast.success("Login Successful!",{
-      position:'top-right'});
-      window.location.href="/display";
-      } catch (error) {
-        console.log(error.message);
-        toast.error(error.message,{
-          position:'top-right'});
-      }
+  const request = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Success");
+      toast.success("Login Successful!", {
+        position: "top-right",
+      });
+      // window.location.href="/display";
+      navigate("/display");
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message, {
+        position: "top-right",
+      });
     }
+  };
 
   return (
-
-<div
-  className="w-80 rounded-lg shadow h-auto p-6 bg-white relative overflow-hidden"
->
-  <div className="flex flex-col justify-center items-center space-y-2">
-    <h2 className="text-2xl font-medium text-slate-700">Login</h2>
-    <p className="text-slate-500">Enter details below.</p>
-  </div>
-  <form onSubmit={request} className="w-full mt-4 space-y-3">
-    <div>
-      <input
-        className="outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300"
-        placeholder="Username"
-        id="username"
-        name="username"
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-
-      />
-    </div>
-    <div>
-      <input
-        className="outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300"
-        placeholder="Password"
-        id="password"
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-    </div>
-    <div className="flex items-center justify-between">
-      <div className="flex items-center">
-        <input
-          className="mr-2 w-4 h-4"
-          id="remember"
-          name="remember"
-          type="checkbox"
-        />
-        <span className="text-slate-500">Remember me </span>
+    <div className="w-80 rounded-lg shadow h-auto p-6 bg-white relative overflow-hidden uplo">
+      <div className="flex flex-col justify-center items-center space-y-2">
+        <h2 className="text-2xl font-medium text-slate-700">Login</h2>
+        <p className="text-slate-500">Enter details below.</p>
       </div>
-      <a className="text-blue-500 font-medium hover:underline" href="#"
-        >Forgot Password</a
-      >
+      <form onSubmit={request} className="w-full mt-4 space-y-3">
+        <div>
+          <input
+            className="outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300"
+            placeholder="Username"
+            id="username"
+            name="username"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className="outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300"
+            placeholder="Password"
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              className="mr-2 w-4 h-4"
+              id="remember"
+              name="remember"
+              type="checkbox"
+            />
+            <span className="text-slate-500">Remember me </span>
+          </div>
+          <a className="text-blue-500 font-medium hover:underline" href="#">
+            Forgot Password
+          </a>
+        </div>
+        <button
+          className="w-full justify-center py-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded-md text-white ring-2"
+          id="login"
+          name="login"
+          type="submit"
+        >
+          login
+        </button>
+        <p className="flex justify-center space-x-1">
+          <span className="text-slate-700"> Have an account? </span>
+          <Link className="text-blue-500 hover:underline" to="/registration">
+            Sign Up
+          </Link>
+        </p>
+      </form>
     </div>
-    <button
-      className="w-full justify-center py-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded-md text-white ring-2"
-      id="login"
-      name="login"
-      type="submit"
-    >
-      login
-    </button>
-    <p className="flex justify-center space-x-1">
-      <span className="text-slate-700"> Have an account? </span>
-      <Link className="text-blue-500 hover:underline" to="/registration">Sign Up</Link>
-    </p>
-  </form>
-</div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,41 +1,43 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import {Link} from "react-router-dom"
-import {auth, db} from "./firebase"
+import { Link, useNavigate } from "react-router-dom";
+import { auth, db } from "./firebase";
 import { setDoc, doc } from "firebase/firestore";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Registration = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
+  const navigate = useNavigate();
 
-  const register=async(e)=>{
-e.preventDefault();
+  const register = async (e) => {
+    e.preventDefault();
 
-try {
- await createUserWithEmailAndPassword(auth, email, password, name, phone)
- const user= auth.currentUser;
- console.log(user);
- toast.success("Registered Successfully!",{
-  position:'top-right'});
- if (user) {
-  await setDoc(doc (db, "Users", user.uid), {
-  email: user.email,
-  name: name,
-  phone: phone,
-  });
- }
-} catch (error) {
-  console.log(error.message);
-  toast.error(error.message,{
-    position:'top-right'});
-}
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log(user);
+      toast.success("Registered Successfully!", {
+        position: 'top-right'
+      });
+      navigate("/login");
+      if (user) {
+        await setDoc(doc(db, "Users", user.uid), {
+          email: user.email,
+          name: name,
+          phone: phone,
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message, {
+        position: 'top-right'
+      });
+    }
   };
-
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -49,29 +51,29 @@ try {
             type="text"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             placeholder="Enter your name"
-            // value={name}
-            onChange={(e)=> setName(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             type="email"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
-            placeholder="Enter youe email"
-            // value={email}
-            onChange={(e)=> setEmail(e.target.value)}
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             placeholder="Enter your password"
-            // value={password}
-            onChange={(e)=> setPassword(e.target.value)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <input
             type="text"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             placeholder="Enter your number"
-            // value={phone}
-            onChange={(e)=> setPhone(e.target.value)}
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
           <button
             type="submit"
@@ -80,10 +82,8 @@ try {
             Register
           </button>
           <p className="text-gray-900 mt-4">
-            {" "}
             Already have an account?{" "}
-         
-              <Link  className="text-sm text-blue-500 -200 hover:underline mt-4" to="/login"> Login</Link>
+            <Link className="text-sm text-blue-500 hover:underline mt-4" to="/login">Login</Link>
           </p>
         </form>
       </div>

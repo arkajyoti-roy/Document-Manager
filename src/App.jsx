b8bc3fd868d406e0a235ea796c16d8406835d042
arkajyoti-roy <arkajyotiroy445@gmail.com>
2024-09-10 17:14:48 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Registration from "./Components/Registration";
 import Login from "./Components/Login";
 import Display from "./Components/Display";
+import { auth } from "./Components/firebase";
+import { useState, useEffect } from "react";
+
+
 function App() {
+
+  const [user, setUser] = useState();
+  useEffect (() => {
+  auth.onAuthStateChanged((user) => {
+  setUser(user);
+  });
+  });
+
+
   return (
-    <>
+     <>
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={} /> */}
-          <Route path="/" element={<Registration />} />
+          <Route path="/" element={user ? <Navigate to="/display" /> : <Login/> } />
           <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/display" element={<Display/>} />
